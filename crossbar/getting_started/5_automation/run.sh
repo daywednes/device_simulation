dc7c16f6bb6f626c8135a87111a981f05480d1bd
Huy-desktop <anhhuy0501@gmail.com>
2020-12-11 17:24:18 +0700

    fix automation

--- crossbar/getting_started/5_automation/run.sh
+++ crossbar/getting_started/5_automation/run.sh
@@ -2,6 +2,4 @@
 
 source ./venv/bin/activate
 
-python client_component_publish.py &
-python client_component_subscribe.py &
-python automation.py && kill $!
+python automation.py
--- crossbar/getting_started/5_automation/sqlalchemy_postgres.py
+++ crossbar/getting_started/5_automation/sqlalchemy_postgres.py
@@ -16,11 +16,22 @@ if __name__ == '__main__':
     db = create_engine_db()
 
     # Create
-    db.execute("CREATE TABLE IF NOT EXISTS automations_entity (id Integer, status text, data text, orgid text, name text, description text, lastTimeUpdate timestamp, lastTimeStartAutomation timestamp, type Integer)")
-    db.execute("INSERT INTO automations_entity (id, status, data, orgid, name, description, lastTimeUpdate, lastTimeStartAutomation, type) VALUES (2,'CreatedAutomation', '{\"id\":\"retejs@0.1.0\",\"nodes\":{}}','4', 'sleep', 'automation3', '2020-10-31 17:45:26', '2020-10-29 17:32:35', 0)")
+    db.execute("DROP TABLE automations_entity")
+    db.execute("CREATE TABLE IF NOT EXISTS automations_entity (id Integer, status text, data text, orgid Integer, name text, description text, lastTimeUpdate timestamp, lastTimeStartAutomation timestamp, type Integer)")
+
+    # Delete
+    db.execute("DELETE FROM automations_entity")
+
+    # Insert
+    db.execute("INSERT INTO automations_entity (id, status, data, orgid, name, description, lastTimeUpdate, lastTimeStartAutomation, type) VALUES (2,'CreatedAutomation', '{\"id\":\"retejs@0.1.0\",\"nodes\":{}}',4, 'sleep', 'automation3', '2020-10-31 17:45:26', '2020-10-29 17:32:35', 0)")
     db.execute(
-        "INSERT INTO automations_entity (id, status, data, orgid, name, description, lastTimeUpdate, lastTimeStartAutomation, type) VALUES (1,'CreatedAutomation', '{\"id\":\"retejs@0.1.0\",\"nodes\":{}}','4', 'away', 'automation3', '2020-10-31 17:45:26', '2020-10-29 17:32:35', 0)")
-    db.execute("INSERT INTO automations_entity (id, status, data, orgid, name, description, lastTimeUpdate, lastTimeStartAutomation, type) VALUES (3,'CreatedAutomation', '{\"id\":\"retejs@0.1.0\",\"nodes\":{}}','4', 'stay', 'automation3', '2020-10-31 17:45:26', '2020-10-29 17:32:35', 0)")
+        "INSERT INTO automations_entity (id, status, data, orgid, name, description, lastTimeUpdate, lastTimeStartAutomation, type) VALUES (1,'CreatedAutomation', '{\"id\":\"retejs@0.1.0\",\"nodes\":{}}',4, 'away', 'automation3', '2020-10-31 17:45:26', '2020-10-29 17:32:35', 0)")
+    db.execute("INSERT INTO automations_entity (id, status, data, orgid, name, description, lastTimeUpdate, lastTimeStartAutomation, type) VALUES (3,'CreatedAutomation', '{\"id\":\"retejs@0.1.0\",\"nodes\":{}}',4, 'stay', 'automation3', '2020-10-31 17:45:26', '2020-10-29 17:32:35', 0)")
+
+    db.execute("INSERT INTO automations_entity (id, status, data, orgid, name, description, lastTimeUpdate, lastTimeStartAutomation, type) VALUES (2,'CreatedAutomation', '{\"id\":\"retejs@0.1.0\",\"nodes\":{}}',5, 'sleep', 'automation3', '2020-10-31 17:45:26', '2020-10-29 17:32:35', 0)")
+    db.execute(
+        "INSERT INTO automations_entity (id, status, data, orgid, name, description, lastTimeUpdate, lastTimeStartAutomation, type) VALUES (1,'CreatedAutomation', '{\"id\":\"retejs@0.1.0\",\"nodes\":{}}',5, 'away', 'automation3', '2020-10-31 17:45:26', '2020-10-29 17:32:35', 0)")
+
 
     # Read
     result_set = db.execute("SELECT * FROM automations_entity")
@@ -30,6 +41,3 @@ if __name__ == '__main__':
     # Update
     #db.execute("UPDATE films SET title='Some2016Film' WHERE year='2016'")
 
-    # Delete
-    #db.execute("DELETE FROM automations_entity")
-    #db.execute("DELETE FROM automations_entity WHERE year='2016'")
